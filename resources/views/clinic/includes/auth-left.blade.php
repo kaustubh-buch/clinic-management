97cdf31a25c84364bb13de20a3798889b535915f
kaustubh.buch <kaustubh.buch@tatvasoft.com>
2025-12-18 16:27:24 +0530

    inital commit

---
+++ resources/views/clinic/includes/auth-left.blade.php
@@ -0,0 +1,16 @@
+<div class="auth-left">
+    <div class="auth-left-inner">
+        <div class="logo-block">
+            <a href="{{ route('home') }}" title="Clinicmanagement">
+                <img src="{{ asset('front/images/clinic-management-logo.svg') }}" alt="Site Logo">
+            </a>
+        </div>
+        <div class="image-wrapper">
+            <img src="{{ asset('front/images/authentication-image.jpg') }}" alt="Login">
+        </div>
+        <div class="content-bock">
+            <p class="h2">Stay Connected with Your Patients</p>
+            <p>Keep your clinic busy with proven, ready-to-use recall SMS templates that bring patients back effortlessly.</p>
+        </div>
+    </div>
+</div>
